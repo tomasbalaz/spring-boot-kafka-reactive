c684e510b37d8daf6adc5c41eba8e12f6640e160
Tomas Balaz <balaztomas0@gmail.com>
2024-01-11 14:06:22 +0100

    Created application.yml for consumer and producer application

--- consumer/src/main/java/sk/balaz/consumer/ConsumerApplication.java
+++ consumer/src/main/java/sk/balaz/consumer/ConsumerApplication.java
@@ -2,6 +2,7 @@ package sk.balaz.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
--- producer/src/main/java/sk/balaz/producer/ProducerApplication.java
+++ producer/src/main/java/sk/balaz/producer/ProducerApplication.java
@@ -2,6 +2,7 @@ package sk.balaz.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ProducerApplication {
     public static void main(String[] args) {
