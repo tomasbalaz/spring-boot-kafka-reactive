fdc94e8694eaa9624256bf704c8a09ecaffa563d
Tomas Balaz <balaztomas0@gmail.com>
2024-01-11 13:58:57 +0100

    Created application spring-boot-kafka-reactive

---
+++ consumer/src/main/java/sk/balaz/consumer/ConsumerApplication.java
@@ -0,0 +1,10 @@
+package sk.balaz.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
